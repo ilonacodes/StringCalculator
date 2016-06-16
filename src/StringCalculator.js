9c1cff9ae0de09095e6a838e0c78389bbd8a47bc
Ilona Demidenko <demiluri@gmail.com>
2016-06-17 00:32:21 +0200

    Implement for any amount of numbers.

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,8 +1,44 @@
-function StringCalculator() {
-
+function StringCalculator(radix, delimiter) {
+  this.radix = radix
+  this.delimiter = delimiter
 }
 
-StringCalculator.prototype.add = function (numbers) {
-  if (numbers == "") return 0
-  return 1
+StringCalculator.Zero = 0
+
+StringCalculator.prototype.add = function(numbers) {
+  if (isEmpty(numbers)) return StringCalculator.Zero
+
+  if (this.includesDelimiter(numbers)) {
+    var splitted = this.splitByDelimiter(numbers)
+    var first = splitted[0]
+    var second = splitted[1]
+
+    return this.parseInt(first) + this.add(second)
+  }
+
+  return this.parseInt(numbers)
+}
+
+StringCalculator.prototype.parseInt = function(string) {
+  return parseInt(string, this.radix)
+}
+
+StringCalculator.prototype.includesDelimiter = function(numbers) {
+  return numbers.includes(this.delimiter)
+}
+
+StringCalculator.prototype.splitByDelimiter = function(numbers) {
+  return split(numbers, this.delimiter)
+}
+
+function isEmpty(numbers) {
+  return numbers == ""
+}
+
+function split(string, delimiter) {
+  var index = string.indexOf(delimiter)
+  return [
+    string.substring(0, index),
+    string.substring(index + 1)
+  ]
 }
