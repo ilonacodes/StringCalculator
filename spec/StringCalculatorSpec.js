1fbcc79115eba3aa4f5d5d50f115aa58c8660edd
Ilona Demidenko <demiluri@gmail.com>
2016-06-19 00:14:27 +0200

    Import to idea

--- spec/StringCalculatorSpec.js
+++ spec/StringCalculatorSpec.js
@@ -1,33 +1,35 @@
-describe("StringCalculator", function() {
-  var comma = ","
-  var stringCalculator = new StringCalculator(10, comma)
+var StringCalculator = require("../src/StringCalculator")
 
-  it("can add numbers", function() {
-    expect(stringCalculator.add("")).toEqual(0)
-  })
+describe("StringCalculator", function () {
+    var comma = ","
+    var stringCalculator = new StringCalculator(10, comma)
 
-  it("can add one number", function() {
-    expect(stringCalculator.add("1")).toEqual(1)
-  })
+    it("can add numbers", function () {
+        expect(stringCalculator.add("")).toEqual(0)
+    })
 
-  it("can add one different number", function() {
-    expect(stringCalculator.add("2")).toEqual(2)
-  })
+    it("can add one number", function () {
+        expect(stringCalculator.add("1")).toEqual(1)
+    })
 
-  it("can add binary number", function() {
-    var binaryStringCalculator = new StringCalculator(2, comma)
-    expect(binaryStringCalculator.add("101")).toEqual(5)
-  })
+    it("can add one different number", function () {
+        expect(stringCalculator.add("2")).toEqual(2)
+    })
 
-  it("can add two numbers", function() {
-    expect(stringCalculator.add("1,2")).toEqual(3)
-  })
+    it("can add binary number", function () {
+        var binaryStringCalculator = new StringCalculator(2, comma)
+        expect(binaryStringCalculator.add("101")).toEqual(5)
+    })
 
-  it("can add two other numbers", function() {
-    expect(stringCalculator.add("3,4")).toEqual(7)
-  })
+    it("can add two numbers", function () {
+        expect(stringCalculator.add("1,2")).toEqual(3)
+    })
 
-  it("can add three numbers", function() {
-    expect(stringCalculator.add("7,3,4,5,8")).toEqual(27)
-  })
+    it("can add two other numbers", function () {
+        expect(stringCalculator.add("3,4")).toEqual(7)
+    })
+
+    it("can add arbitrary amount of numbers", function () {
+        expect(stringCalculator.add("7,3,4,5,8")).toEqual(27)
+    })
 })
--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,44 +1,49 @@
 function StringCalculator(radix, delimiter) {
-  this.radix = radix
-  this.delimiter = delimiter
+    this.radix = radix
+    this.delimiter = delimiter
 }
 
 StringCalculator.Zero = 0
 
-StringCalculator.prototype.add = function(numbers) {
-  if (isEmpty(numbers)) return StringCalculator.Zero
+StringCalculator.prototype.add = function (numbers) {
+    if (isEmpty(numbers)) return StringCalculator.Zero
 
-  if (this.includesDelimiter(numbers)) {
-    var splitted = this.splitByDelimiter(numbers)
-    var first = splitted[0]
-    var second = splitted[1]
+    if (this.includesDelimiter(numbers)) {
+        var splitted = this.splitByDelimiter(numbers)
+        var first = splitted[0]
+        var second = splitted[1]
 
-    return this.parseInt(first) + this.add(second)
-  }
+        return this.parseInt(first) + this.add(second)
+    }
 
-  return this.parseInt(numbers)
+    return this.parseInt(numbers)
 }
 
-StringCalculator.prototype.parseInt = function(string) {
-  return parseInt(string, this.radix)
+StringCalculator.prototype.parseInt = function (string) {
+    return parseInt(string, this.radix)
 }
 
-StringCalculator.prototype.includesDelimiter = function(numbers) {
-  return numbers.includes(this.delimiter)
+StringCalculator.prototype.includesDelimiter = function (numbers) {
+    return numbers.includes(this.delimiter)
 }
 
-StringCalculator.prototype.splitByDelimiter = function(numbers) {
-  return split(numbers, this.delimiter)
+StringCalculator.prototype.splitByDelimiter = function (numbers) {
+    return split(numbers, this.delimiter)
 }
 
 function isEmpty(numbers) {
-  return numbers == ""
+    return numbers == ""
 }
 
 function split(string, delimiter) {
-  var index = string.indexOf(delimiter)
-  return [
-    string.substring(0, index),
-    string.substring(index + 1)
-  ]
+    var index = string.indexOf(delimiter)
+    return [
+        string.substring(0, index),
+        string.substring(index + 1)
+    ]
+}
+
+
+if (typeof module != "undefined") {
+    module.exports = StringCalculator
 }
